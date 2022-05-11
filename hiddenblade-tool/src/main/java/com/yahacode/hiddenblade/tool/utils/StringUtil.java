14e8f87a709f6c43d541927d74b89f459c994bee
zeoly <zeoly100@163.com>
2022-05-11 19:40:24 +0800

    tool, String add pattern utils

--- hiddenblade-tool/src/main/java/com/yahacode/hiddenblade/tool/utils/StringUtil.java
+++ hiddenblade-tool/src/main/java/com/yahacode/hiddenblade/tool/utils/StringUtil.java
@@ -3,6 +3,8 @@ package com.yahacode.hiddenblade.tool.utils;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.regex.Pattern;
+
 /**
  * String type utilities
  *
@@ -11,6 +13,26 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class StringUtil extends StringUtils {
 
+    private static final Pattern numericPattern = Pattern.compile("[+-]?[0-9]+(\\.[0-9]+)?");
+
+    private static final Pattern yearPattern = Pattern.compile("(19|20|21)\\d{2}");
+
+    private static final Pattern yearMonthPattern = Pattern.compile("(19|20|21)\\d{2}[-|/]((0[1-9])|(1[0-2]))");
+
+    private static final Pattern datePattern = Pattern.compile("(19|20|21)\\d{2}[-|/]((0[1-9])|(1[0-2]))[-|/]((0[1-9])|([12][0-9])|(3[0-1]))");
+
+    private static final Pattern hourMinutePattern = Pattern.compile("(([01][0-9])|(2[0-3]))(:[0-5][0-9])");
+
+    private static final Pattern timePattern = Pattern.compile("(([01][0-9])|(2[0-3]))(:[0-5][0-9]){2}");
+
+    private static final Pattern vinPattern = Pattern.compile("[A-HJ-NPR-Z\\d]{17}");
+
+    private static final Pattern plateNoOriginPattern = Pattern.compile("^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使][A-Z][A-HJ-NP-Z0-9]{4}[A-Z0-9领挂学警港澳]$");
+
+    private static final Pattern plateNoGreenPattern = Pattern.compile("^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼][A-Z][A-HJ-NP-Z0-9]{6}$");
+
+    private static final Pattern plateNoWJPattern = Pattern.compile("^WJ[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼][0-9A-Z]{5}");
+
     /**
      * not only digit char, include symbol and dot
      *
@@ -18,10 +40,7 @@ public class StringUtil extends StringUtils {
      * @return true if is numeric format
      */
     public static Boolean isNumericFormat(String str) {
-        if (isEmpty(str)) {
-            return false;
-        }
-        return str.matches("[+-]?[0-9]+(\\.[0-9]+)?");
+        return isPattern(str, numericPattern);
     }
 
     /**
@@ -48,6 +67,78 @@ public class StringUtil extends StringUtils {
         }
         JsonNode jsonNode = JsonUtil.toJsonNode(str);
         return jsonNode != null;
+    }
+
+    public static boolean isPattern(String str, String pattern) {
+        Pattern p = Pattern.compile(pattern);
+        return isPattern(str, p);
+    }
+
+    private static boolean isPattern(String str, Pattern p) {
+        if (str == null) {
+            return false;
+        }
+        if (p.matcher(str).matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isYear(String str) {
+        return isPattern(str, yearPattern);
+    }
+
+    public static boolean isYearMonth(String str) {
+        return isPattern(str, yearMonthPattern);
+    }
+
+    public static boolean isDate(String str) {
+        return isPattern(str, datePattern);
+    }
+
+    public static boolean isHourMinute(String str) {
+        return isPattern(str, hourMinutePattern);
+    }
+
+    public static boolean isTime(String str) {
+        return isPattern(str, timePattern);
+    }
+
+    public static boolean isDateHourMinute(String str) {
+        String[] part = str.split(" ");
+        if (part.length != 2) {
+            return false;
+        }
+        return isDate(part[0]) && isHourMinute(part[1]);
+    }
+
+    public static boolean isDateTime(String str) {
+        String[] part = str.split(" ");
+        if (part.length != 2) {
+            return false;
+        }
+        return isDate(part[0]) && isTime(part[1]);
+    }
+
+    public static boolean isVIN(String str) {
+        return isPattern(str, vinPattern);
+    }
+
+    public static boolean isOriginPlateNo(String str) {
+        return isPattern(str, plateNoOriginPattern);
+    }
+
+    public static boolean isGreenPlateNo(String str) {
+        return isPattern(str, plateNoGreenPattern);
+    }
+
+    public static boolean isWJPlateNo(String str) {
+        return isPattern(str, plateNoWJPattern);
+    }
+
+    public static boolean isPlateNo(String str) {
+        return isOriginPlateNo(str) || isGreenPlateNo(str) || isWJPlateNo(str);
     }
 
     public static String toHex2(int i) {
---
+++ hiddenblade-tool/src/test/java/com/yahacode/hiddenblade/tool/utils/StringUtilTest.java
@@ -0,0 +1,71 @@
+package com.yahacode.hiddenblade.tool.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilTest {
+
+    @Test
+    void isYear() {
+        assertTrue(StringUtil.isYear("2021"));
+    }
+
+    @Test
+    void isYearMonth(){
+        assertTrue(StringUtil.isYearMonth("2021-02"));
+    }
+
+
+    @Test
+    void isHourMinute() {
+        assertTrue(StringUtil.isHourMinute("12:12"));
+        assertFalse(StringUtil.isHourMinute("12:12:12"));
+        assertFalse(StringUtil.isHourMinute("24:12"));
+    }
+
+    @Test
+    void isTime() {
+        assertTrue(StringUtil.isTime("00:01:00"));
+        assertFalse(StringUtil.isTime("24:01:00"));
+        assertFalse(StringUtil.isTime("13:60:00"));
+    }
+
+    @Test
+    void isDateHourMinute() {
+        assertTrue(StringUtil.isDateHourMinute("2021-01-01 23:12"));
+        assertFalse(StringUtil.isDateHourMinute("2021-01-01 23:12:12"));
+        assertFalse(StringUtil.isDateHourMinute("2021-01-01"));
+    }
+
+    @Test
+    void isDateTime() {
+        assertTrue(StringUtil.isDateTime("2021-01-01 23:12:12"));
+        assertFalse(StringUtil.isDateTime("2021/00/01 23:12:34"));
+        assertFalse(StringUtil.isDateTime("2021-01-01 23:12"));
+        assertFalse(StringUtil.isDateTime("2021-01-32 23:12:12"));
+        assertFalse(StringUtil.isDateTime("2021-00-01 23:12:34"));
+        assertFalse(StringUtil.isDateTime("2021-13-01 23:12:34"));
+    }
+
+    @Test
+    void isNumericFormat() {
+        assertTrue(StringUtil.isNumericFormat("12.3"));
+    }
+
+    @Test
+    void isVIN() {
+        assertTrue(StringUtil.isVIN("LFV3A28W4L3718113"));
+    }
+
+    @Test
+    void isPlateNo() {
+        assertTrue(StringUtil.isPlateNo("粤B12345"));
+        assertTrue(StringUtil.isPlateNo("粤BD12345"));
+        assertTrue(StringUtil.isPlateNo("粤GGSU9港"));
+        assertTrue(StringUtil.isPlateNo("粤X58KC学"));
+        assertTrue(StringUtil.isPlateNo("使LXFKQZ"));
+        assertTrue(StringUtil.isPlateNo("晋DDRE5领"));
+        assertTrue(StringUtil.isPlateNo("WJ晋12345"));
+    }
+}
