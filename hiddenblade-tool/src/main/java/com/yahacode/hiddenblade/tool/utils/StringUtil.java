c2bb90756945dda0426081342e2cc799cf746601
zeoly <zeoly100@163.com>
2022-06-06 14:56:24 +0800

    tool 0.3.0 release, add StringUtil similarity

--- hiddenblade-tool/src/main/java/com/yahacode/hiddenblade/tool/utils/StringUtil.java
+++ hiddenblade-tool/src/main/java/com/yahacode/hiddenblade/tool/utils/StringUtil.java
@@ -161,4 +161,81 @@ public class StringUtil extends StringUtils {
         }
         return String.format("%08x", i).toUpperCase();
     }
+
+    public static double similarDegree(String strA, String strB) {
+        String newStrA = removeSign(strA);
+        String newStrB = removeSign(strB);
+        int max = Math.max(newStrA.length(), newStrB.length());
+        int common = longestCommonSubstring(newStrA, newStrB).length();
+        return common * 1.0 / max;
+    }
+
+    public static double similarDegreeMin(String strA, String strB) {
+        String newStrA = removeSign(strA);
+        String newStrB = removeSign(strB);
+        int min = Math.min(newStrA.length(), newStrB.length());
+        int common = longestCommonSubstring(newStrA, newStrB).length();
+        return common * 1.0 / min;
+    }
+
+    private static String removeSign(String str) {
+        StringBuffer sb = new StringBuffer();
+        for (char item : str.toCharArray()) {
+            if (charReg(item)) {
+                sb.append(item);
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean charReg(char charValue) {
+        return (charValue >= 0x4E00 && charValue <= 0X9FA5) || (charValue >= 'a' && charValue <= 'z') || (charValue >= 'A' && charValue <= 'Z') || (charValue >= '0' && charValue <= '9');
+    }
+
+    public static String longestCommonSubstring(String strA, String strB) {
+        if (strA.length() >= strB.length()) {
+            return longestCommonSubstringOrdered(strA, strB);
+        } else {
+            return longestCommonSubstringOrdered(strB, strA);
+        }
+    }
+
+    private static String longestCommonSubstringOrdered(String strLong, String strShort) {
+        char[] chars_strA = strLong.toCharArray();
+        char[] chars_strB = strShort.toCharArray();
+        int m = chars_strA.length;
+        int n = chars_strB.length;
+        int[][] matrix = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (chars_strA[i - 1] == chars_strB[j - 1]) {
+                    matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                } else {
+                    matrix[i][j] = Math.max(matrix[i][j - 1], matrix[i - 1][j]);
+                }
+            }
+        }
+        char[] result = new char[matrix[m][n]];
+        int currentIndex = result.length - 1;
+        while (matrix[m][n] != 0) {
+            if (matrix[n] == matrix[n - 1]) {
+                n--;
+            } else if (matrix[m][n] == matrix[m - 1][n]) {
+                m--;
+            } else {
+                result[currentIndex] = chars_strA[m - 1];
+                currentIndex--;
+                n--;
+                m--;
+            }
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(similarDegreeMin("中国石化东阳白鹤（人民路）", "金东阳白鹤"));
+        System.out.println(similarDegreeMin("中国石化（光辉街）", "衡阳市光辉路"));
+        System.out.println(similarDegreeMin("中国石油（五公里站）", "重庆销售五公里"));
+
+    }
 }
